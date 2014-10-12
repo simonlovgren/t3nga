bf75584e3bd15c7aaf99cc387dbcfceee526b147
Simon Lövgren <lovgren.simon@gmail.com>
2014-10-12 13:06:58 +0200

    First commit

---
+++ network.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import socket
+
+
+#WIP#
+class NetworkListener:
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
+        self.listen()
+        
+    def listen(self):
+        # Collect latest data and update data variable
+        self.data = "Simulated data"
+        print("Listening")
+    
+    def getData(self):
+        return self.data
+    
+#nl = NetworkListener("192.168.1.23", "22500");
+#print(nl.getData())
+
+
+## Taken from: https://docs.python.org/3.1/howto/sockets.html
+class mysocket:
+    """demonstration class only
+      - coded for clarity, not efficiency
+    """
+
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < MSGLEN:
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def myreceive(self):
+        msg = ''
+        while len(msg) < MSGLEN:
+            chunk = self.sock.recv(MSGLEN-len(msg))
+            if chunk == '':
+                raise RuntimeError("socket connection broken")
+            msg = msg + chunk
+        return msg
