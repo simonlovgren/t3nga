e896742171d55fd86966867ef9d313239c5b43ac
Simon Lövgren <lovgren.simon@gmail.com>
2014-10-21 00:46:53 +0200

    Tweaked and optimized game.py

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 from random import randint
 from gui import *
+import sys
+import time
 
 class Game:
     #def __init__(self):
@@ -27,10 +29,10 @@ class Game:
 
         #starta ett nytt spel eller avsluta programmet
         if ny == 0:
-            self.gui.close()
-            self.start()
+            #self.gui.close()  # Changed so main menu displays in same window
+            self.start(True)
         elif ny == 1:
-            exit()
+            sys.exit()
 
     def winRow(self, board, spelare, koll, xInRow):
 
@@ -38,12 +40,13 @@ class Game:
         # är lika med xInRow så har någon vunnit och användaren frågas om nytt spel
 
         counter = 0
-        for i in range(0, len(koll)):
-            if board[koll[i]] is not spelare:
+        for i in koll:
+            if board[i] is not spelare:
                 counter = 0
             else:
                 counter += 1
             if counter >= xInRow:
+                self.msleep(250) # Sleep to allow user to see final screen
                 self.gui.setStatus('grattis ' + spelare + ' du har vunnit, vill du spela igen?')
                 self.gui.update()
                 self.newGame()
@@ -104,31 +107,31 @@ class Game:
 
         #om det inte finns någon tom ruta, dvs ingen ruta som innehåller '*' så är spelet oavgjort. Informera spelaren och fråga om en ny omgång.
         if '*' not in board:
+            self.msleep(250) # Sleep to allow user to see final screen
             self.gui.setStatus('oavgjort, vill du spela igen?')
             self.gui.update()
-            self.newGame()
-
-    def playerAction(self,spelare):
-        
-        self.gui.setStatus('spelare ' + spelare + ' välj en tom plats på spelplanen igenom att trycka på den')
-        return self.gui.waitForBoard()
-        
-        
+            self.newGame()        
         
                 
     #Människor gör ett drag
     def man(self, spelare, board):
-    
         # player action väljer ut var spelaren klickar. Ifall det är en tom plats blir den nu spelarens.
-        var = self.playerAction(spelare)
-        if board[var] != '*':
-            var = self.playerAction(spelare)
+        self.gui.setStatus('spelare ' + spelare + ' välj en tom plats på spelplanen igenom att trycka på den')
+        self.gui.update()
 
-        else:
-            board[var] = spelare
-            return board
+        while True:
+            var = self.gui.waitForBoard()
+            if board[var] == '*':
+                break
+            else:
+                self.gui.setStatus('Rutan är upptagen, välj en annan ruta.')
+                self.gui.update()
+        
+        board[var] = spelare
+        return board
 
     def twoRow(self, board, koll, testa):
+        # Värderar segment beroende på närliggande symboler i rad
         spe = 0
         tom = 0
         for i in koll:
@@ -155,18 +158,20 @@ class Game:
                 else:
                     testa = 'x'
 
+            # Horizontal lines
             for i in [0, 3, 6]:
                 koll = [i, i + 1, i + 2]
                 if self.twoRow(board, koll, testa) >= 0:
                     board[self.twoRow(board, koll, testa)] = spelare
                     return board
-
+            # Vertical lines
             for i in [0, 1, 2]:
                 koll = [i, i + 3, i + 6]
                 if self.twoRow(board, koll, testa) >= 0:
                     board[self.twoRow(board, koll, testa)] = spelare
                     return board
-        
+            
+            # Diagonals
             if self.twoRow(board, [0, 4, 8], testa) >= 0:
                 board[self.twoRow(board, [0, 4, 8], testa)] = spelare
                 return board
@@ -182,61 +187,46 @@ class Game:
         board[plats[randint(0, len(plats) -1)]] = spelare
         return board
     
-    def start(self):
+    def start(self, newGame=False):
 
         #definera spelplanen
         tur = 0
         spelare = 'x'
-        self.gui = GUI()
-        self.gui.createWindow()
-        self.gui.createStatus("välkommen till luffarschack")
-        self.gui.createMenu(["Human vs AI", "PvP"], 100)
-        self.gui.update()
-        select = self.gui.waitForMenu()
+
+        # Check if returning to menu
+        if not newGame:
+            # Starting for the first time
+            self.gui = GUI()
+            self.gui.createWindow()
+            self.gui.createStatus("välkommen till luffarschack")
+        else:
+            # Returning to main menu
+            self.gui.setStatus("välkommen till luffarschack")
+
+        # Display main menu
+        select = self.mainMenu()
         
+        # Respond to menu selection
         if select == 0:
+            # Play against AI @ 3x3 grid
             intelligens = [0, 1]
             width = 3
             xInRow = 3
         
         elif select == 1:
+            # PVP
             intelligens = [0, 0]
-            self.gui.setStatus("Välj hur stort spelbräde du vill ha")
-            self.gui.createMenu(["3x3", "4x4", "5x5", "6x6", "7x7", "8x8", "9x9", "10x10"], 100)
-            self.gui.update()
-            width = self.gui.waitForMenu() + 3
-
+            # Display board selection
+            width = self.boardMenu()
             #välj hur många i rad man ska ha för att vinna
-            self.gui.setStatus("välj hur många i rad man ska ha för att vinna")
-            
-            if width == 3:
-                self.gui.createMenu(["2", "3"], 100)
+            xInRow = self.xInRowMenu(width)
 
-            elif width == 4:
-                self.gui.createMenu(["2", "3", "4"], 100)
+        elif select == 2:
+            # Close game
+            sys.exit()
 
-            elif width == 5:
-                self.gui.createMenu(["2", "3", "4", "5"], 100)
-
-            elif width == 6:
-                self.gui.createMenu(["2", "3", "4", "5", "6"], 100)
-
-            elif width == 7:
-                self.gui.createMenu(["2", "3", "4", "5", "6", "7"], 100)
-
-            elif width == 8:
-                self.gui.createMenu(["2", "3", "4", "5", "6", "7", "8"], 100)
-
-            elif width == 9:
-                self.gui.createMenu(["2", "3", "4", "5", "6", "7", "8", "9",], 100)
-
-            elif width == 10:
-                self.gui.createMenu(["2", "3", "4", "5", "6", "7", "8", "9", "10" ], 100)
-
-            self.gui.update()
-            xInRow = self.gui.waitForMenu() + 2
         #visa spelplanen för spelarna
-        board = ['*']*(width*width)
+        board = ['*']*(width**2)
         self.playingField(board)
         self.gui.createBoard([500,500],[width,width])
         self.gui.update()
@@ -250,15 +240,44 @@ class Game:
                 spelare = 'x'
             else:
                 spelare = 'o'
-
         
             #Gör ett drag
             if intelligens[tur] == 0:
+                # Vänta på spelare
                 board = self.man(spelare, board)
             else:
+                # AI
+                self.msleep(randint(50,250)) # Sleep a couple milliseconds to "emulate" a real person
                 board = self.ai(spelare, board)
 
             #visa spelplanen efter spelarens drag, kolla ifall spelaren har vunnit och öka tur med 1
             self.playingField(board)
             self.winCheck(board, spelare, width, xInRow)
             tur += 1
+
+
+
+
+    ###  Menues
+    def mainMenu(self):
+        self.gui.createMenu(["Human vs AI", "PvP", "Avsluta spelet"], 100)
+        self.gui.update()
+        return self.gui.waitForMenu()
+
+    def boardMenu(self):
+        self.gui.setStatus("Välj hur stort spelbräde du vill ha")
+        self.gui.createMenu(["3x3", "4x4", "5x5", "6x6", "7x7", "8x8", "9x9", "10x10"], 100)
+        self.gui.update()
+        return self.gui.waitForMenu() + 3
+
+    def xInRowMenu(self, width):
+        self.gui.setStatus("välj hur många i rad man ska ha för att vinna")
+        self.gui.createMenu(list(map(str,range(2,width+1))), 100)
+        self.gui.update()
+        return self.gui.waitForMenu() + 2
+
+    ### Millisleep
+    def msleep(self, sleep):
+        ref, now = time.clock(), time.clock() # Set reference to now
+        while (now - ref) < (sleep/1000):
+            now = time.clock()
